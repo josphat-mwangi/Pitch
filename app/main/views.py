42df3237df9c3a287aaa3112e246fc6c747fd50e
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-07-16 12:22:33 +0300

    Update the navbar

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
 from flask_login import login_required
 from ..models import User,Pitch,Comment,Upvotes,Downvotes
 from . import main
@@ -13,7 +13,11 @@ def index():
     title = 'Welcome to Pitch'
     return render_template('index.html',title=title)
 
-# @main.route('/add_new',methods = ['GET','POST'])
-# @login_required
-# def new_pitch():
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    
+    if user is None:
+        abort(404)
+    return render_template("profile/profile.html",user = user)
 
