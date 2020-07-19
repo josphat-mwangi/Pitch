c0aac84f8ced33f16eb55f65e36c7a274569af05
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-07-19 07:39:50 +0300

    update models.py

--- app/models.py
+++ app/models.py
@@ -15,137 +15,87 @@ class User(UserMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), index=True)
+    firstname = db.Column(db.String(255))
+    lastname = db.Column(db.String(255))
     email = db.Column(db.String(255), unique=True, index=True)
-    bio = db.Column(db.String(255))
-    profile_pic_path = db.Column(db.String())
-    password_secure = db.Column(db.String(255))
-    password_hash = db.Column(db.String(255))
-    pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
-    comment = db.relationship('Comments', backref='user', lazy='dynamic')
-    vote = db.relationship('Votes', backref='user', lazy='dynamic')
+    bio = db.Column(db.String(5000))
+    profile_pic_path = db.Column(db.String)
+    pass_secure = db.Column(db.String(255))
+    date_joined = db.Column(db.DateTime, default=datetime.utcnow)
+
+    pitches = db.relationship('Pitch', backref='user', lazy="dynamic")
+
+    comments = db.relationship('Comment', backref='user', lazy="dynamic")
 
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
 
     @password.setter
-    def password(self,password):
-        self.password_secure = generate_password_hash(password)
-    def verify_password(self,password):
-        return check_password_hash(self.password_secure, password)
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
 
     def __repr__(self):
         return f'User {self.username}'
 
-#category model
-
-
-class PitchCategory(db.Model):
-
-    __tablename__ = 'categories'
-
-    # table columns
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    description = db.Column(db.String(255))
-
-    # save pitches
-    def save_category(self):
-        db.session.add(self)
-        db.session.commit()
-
-    @classmethod
-    def get_categories(cls):
-        categories = PitchCategory.query.all()
-        return categories
-
-#pitches class
-
 
 class Pitch(db.Model):
-    """
-    List of pitches in each category 
-    """
-
     __tablename__ = 'pitches'
 
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String())
-    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"))
+    pitch_title = db.Column(db.String)
+    pitch_content = db.Column(db.String(1000))
+    category = db.Column(db.String)
+    posted = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    comment = db.relationship("Comments", backref="pitches", lazy="dynamic")
-    vote = db.relationship("Votes", backref="pitches", lazy="dynamic")
+    likes = db.Column(db.Integer)
+    dislikes = db.Column(db.Integer)
+
+    comments = db.relationship('Comment', backref='pitch_id', lazy="dynamic")
 
     def save_pitch(self):
-        """
-        Save the pitches 
-        """
         db.session.add(self)
         db.session.commit()
 
     @classmethod
-    def clear_pitches(cls):
-        Pitch.all_pitches.clear()
-
-    # display pitches
-    @classmethod
-    def get_pitches(cls, id):
-        pitches = Pitch.query.filter_by(category_id=id).all()
+    def get_pitches(cls, category):
+        pitches = Pitch.query.filter_by(category=category).all()
         return pitches
 
-# comments
+    @classmethod
+    def get_pitch(cls, id):
+        pitch = Pitch.query.filter_by(id=id).first()
+
+        return pitch
+
+    @classmethod
+    def count_pitches(cls, uname):
+        user = User.query.filter_by(username=uname).first()
+        pitches = Pitch.query.filter_by(user_id=user.id).all()
+
+        pitches_count = 0
+        for pitch in pitches:
+            pitches_count += 1
+
+        return pitches_count
 
 
-class Comments(db.Model):
-    """
-    User comment model for each pitch 
-    """
-
+class Comment(db.Model):
     __tablename__ = 'comments'
 
-    # add columns
-    id = db.Column(db. Integer, primary_key=True)
-    opinion = db.Column(db.String(255))
-    time_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(1000))
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    pitches_id = db.Column(db.Integer, db.ForeignKey("pitches.id"))
+    pitch = db.Column(db.Integer, db.ForeignKey("pitches.id"))
 
     def save_comment(self):
-        """
-        Save the Comments/comments per pitch
-        """
         db.session.add(self)
         db.session.commit()
 
     @classmethod
-    def get_comments(self, id):
-        comment = Comments.query.order_by(
-            Comments.time_posted.desc()).filter_by(pitches_id=id).all()
-        return comment
-
-#votes
-
-
-class Votes(db.Model):
-    """
-    class to model votes
-    """
-    __tablename__ = 'votes'
-
-    id = db.Column(db. Integer, primary_key=True)
-    vote = db.Column(db.Integer)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    pitches_id = db.Column(db.Integer, db.ForeignKey("pitches.id"))
-
-    def save_vote(self):
-        db.session.add(self)
-        db.session.commit()
-
-    @classmethod
-    def get_votes(cls, user_id, pitches_id):
-        votes = Votes.query.filter_by(
-            user_id=user_id, pitches_id=pitches_id).all()
-        return votes
-
-    def __repr__(self):
-        return f'{self.vote}:{self.user_id}:{self.pitches_id}'
+    def get_comments(cls, pitch):
+        comments = Comment.query.filter_by(pitch_id=pitch).all()
+        return comments
